06b8e2cc0a1de4bb2b00bd1567f31836fc28f844
Uddeepta Raaj Kashyap <uddeeptaraajkashyap@gmail.com>
2024-02-27 00:50:16 +0530

    added tests for search func

--- src/test/java/ideario/PageFunctionalityTest.java
+++ src/test/java/ideario/PageFunctionalityTest.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.*;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 
 public class PageFunctionalityTest extends BaseTestClass {
 
@@ -44,35 +47,8 @@ public class PageFunctionalityTest extends BaseTestClass {
         return count;
     }
 
-    @Test
-    public void addNoteUsingCreateANote() throws InterruptedException {
-
-        Thread.sleep(5000);
-
-        Integer count = getCountOfDocuments(By.xpath("//*[@aria-label='Document-item-listicle']"));
-
-        logger.info("Adding a note using Create a note button.");
-
-        helper.isElementDisplayed(By.xpath("//button[text()='Create a note']"));
-        helper.clickElement(By.xpath("//button[text()='Create a note']"));
-
-        logger.info("Verifying if note is added.");
-
-        String title = wait
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(), 'Untitled')]")))
-                .getText();
-
-        logger.info("Title of the note is: " + title);
-
-        softAssert.assertEquals(title, "Untitled", "Note is not added.");
-
-        Integer newCount = getCountOfDocuments(By.xpath("//*[@aria-label='Document-item-listicle']"));
-
-        softAssert.assertEquals(newCount, count + 1, "Note is not added.");
-    }
-
-    @Test
-    public void addNoteUsingAddAPageButton() throws InterruptedException {
+    @Test(groups = "PageFunctionalityTests")
+    public void addPage() throws InterruptedException {
 
         Thread.sleep(5000);
 
@@ -94,5 +70,66 @@ public class PageFunctionalityTest extends BaseTestClass {
         Integer newCount = getCountOfDocuments(By.xpath("//*[@aria-label='Document-item-listicle']"));
 
         softAssert.assertEquals(newCount, count + 1, "Note is not added.");
+    }
+
+    @Test(groups = "PageFunctionalityTests", dependsOnMethods = "addPage")
+    public void deleteTopMostPage() throws InterruptedException {
+        logger.info("Deleting top most page.");
+
+        Thread.sleep(2000);
+
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@aria-label='Document-item-listicle']")));
+
+        var count = getCountOfDocuments(By.xpath("//*[@aria-label='Document-item-listicle']"));
+
+        logger.info("Count of documents before deleting: " + count);
+
+        WebElement element = driver.findElement(By.cssSelector("#radix-\\:rc\\:"));
+        element.click();
+
+        List<WebElement> menuItems = driver.findElements(
+                By.cssSelector("div[role='menuitem'][class*='relative flex cursor-default select-none items-center']"));
+
+        for (WebElement item : menuItems) {
+            if (item.getText().contains("Delete")) {
+                item.click();
+                break;
+            }
+        }
+
+        Thread.sleep(1500);
+        softAssert.assertTrue(driver.getPageSource().contains("Note moved to trash!"), "Note is not deleted successfully.");
+        
+
+        logger.info("Verifying if top most page is deleted.");
+
+
+        Thread.sleep(2000);
+        var newCount = getCountOfDocuments(By.xpath("//*[@aria-label='Document-item-listicle']"));
+
+        softAssert.assertEquals(newCount, count - 1, "Top most page is not deleted.");
+
+        softAssert.assertAll();
+    }
+
+    @Parameters("searchText")
+    @Test(groups = "PageFunctionalityTests", dependsOnMethods = "deleteTopMostPage")
+    void checkSearch(String searchText) {
+
+        logger.info("Checking search functionality.");
+
+        Actions actions = new Actions(driver);
+        actions.keyDown(Keys.CONTROL).sendKeys("k").keyUp(Keys.CONTROL).perform();
+
+        driver.findElement(By.ByTagName.tagName("input")).sendKeys(searchText);
+
+
+        var option = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[data-selected='true']")));
+        option.click();
+
+        var searchTextHeader = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(), '" + searchText + "')]")));
+        
+        softAssert.assertTrue(searchTextHeader.isDisplayed(), "Search is not working.");
     }
 }
